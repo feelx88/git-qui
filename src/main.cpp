2457b9d612691fb7038c24ce5b75692bf53ebf37
Felix Müller <feelx88@googlemail.com>
2017-05-22 06:44:17 +0200

    Added command line parsing for switching git implementations.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <QDir>
 #include <QQmlContext>
 #include <QQuickStyle>
+#include <QCommandLineParser>
 
 #include <git/gitfile.h>
 #include <git/gitdiffline.h>
@@ -13,14 +14,34 @@
 int main(int argc, char *argv[])
 {
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QCoreApplication::setApplicationName("git QUI");
+  QCoreApplication::setApplicationVersion("0.1.0");
   QGuiApplication app(argc, argv);
 
-  GitManager manager(&app);
-  manager.connect(&manager, &GitManager::gitError, [&](const QString &message){
+  QCommandLineParser parser;
+  parser.setApplicationDescription("QML UI for git.");
+  parser.addHelpOption();
+  parser.addVersionOption();
+  parser.addPositionalArgument("<working directory>", "Git working directory.");
+
+  // A boolean option with a single name (-p)
+  QCommandLineOption gitImpl({"L", "libgit2"}, "Use libgit2 implementation instead of parsing git output");
+  parser.addOption(gitImpl);
+
+  parser.process(app);
+
+  AGitManager *manager = nullptr;
+
+  if(parser.isSet(gitImpl))
+  {
+    manager = new libgit2::GitManager(&app);
+  }
+
+  manager->connect(manager, &AGitManager::gitError, [&](const QString &message){
     std::cout << message.toStdString() << std::endl;
   });
-  manager.init();
-  manager.openRepository(QDir::currentPath());
+  manager->init();
+  manager->openRepository(QDir::currentPath());
 
   qmlRegisterType<GitFile>("de.feelx88.GitFile", 1, 0, "GitFile");
   qmlRegisterType<GitDiffLine>("de.feelx88.GitDiffLine", 1, 0, "GitDiffLine");
@@ -28,7 +49,7 @@ int main(int argc, char *argv[])
   QQuickStyle::setFallbackStyle("Material");
 
   QQmlApplicationEngine engine;
-  engine.rootContext()->setContextProperty("gitManager", &manager);
+  engine.rootContext()->setContextProperty("gitManager", manager);
   engine.load(QUrl(QCoreApplication::applicationDirPath() + QLatin1String("/ui/default/main.qml")));
 
   return app.exec();
