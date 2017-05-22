56ebf9c799a3cdcc268d9da151cc1336babc6995
Felix Müller <feelx88@googlemail.com>
2017-05-22 11:47:34 +0200

    Implemented some git-binary-based functionality.

--- src/git/git-bin/gitbingitmanager.cpp
+++ src/git/git-bin/gitbingitmanager.cpp
@@ -22,7 +22,7 @@ void gitBin::GitManager::init()
 {
 }
 
-void gitBin::GitManager::openRepository(const QString &path)
+void gitBin::GitManager::openRepository(const QString &)
 {
 }
 
@@ -30,19 +30,128 @@ QList<GitFile *> gitBin::GitManager::status()
 {
   _impl->process->setArguments({"status", "--porcelain=v1"});
   _impl->process->start(QIODevice::ReadOnly);
+  _impl->process->waitForFinished();
   QByteArray output = _impl->process->readLine(1024);
+
+  QList<GitFile*> list;
 
   while (output.length() > 0)
   {
-    emit gitError(QString(output));
+    GitFile *file = new GitFile(this);
+
+    file->staged = (output.at(0) != ' ');
+    file->unstaged = (output.at(1) != ' ');
+    file->path = output.right(output.length() - 3).trimmed();
+
+    switch(output.at(0))
+    {
+    case 'M':
+      file->modified = true;
+      break;
+    case 'D':
+      file->deleted = true;
+      break;
+    case 'R':
+    case 'C':
+    default:
+      break;
+    }
+
+    switch(output.at(1))
+    {
+    case 'M':
+      file->modified = true;
+      break;
+    case 'D':
+      file->deleted = true;
+      break;
+    case 'R':
+    case 'C':
+    default:
+      break;
+    }
+
+    list.append(file);
+
+    output = _impl->process->readLine(1024);
   }
 
-  return {new GitFile};
+  return list;
 }
 
 QList<GitDiffLine *> gitBin::GitManager::diffPath(const QString &path, bool diffStaged)
 {
-  return {new GitDiffLine};
+  if(diffStaged)
+  {
+    _impl->process->setArguments({"diff", "--cached", "--", path});
+  }
+  else
+  {
+    _impl->process->setArguments({"diff", "--", path});
+  }
+  _impl->process->start(QIODevice::ReadOnly);
+  _impl->process->waitForFinished();
+  QByteArray output = _impl->process->readLine(1024);
+  QRegExp regex("@* \\-(\\d+),.* \\+(\\d+),.*");
+  QStringList lineNos;
+
+  QList<GitDiffLine*> list;
+
+  int lineNoOld = -1;
+  int lineNoNew = -1;
+
+  while(output.length() > 0)
+  {
+    GitDiffLine *line = new GitDiffLine(this);
+    line->oldLine = lineNoOld;
+    line->newLine = lineNoNew;
+    line->content = output.trimmed();
+    line->type = GitDiffLine::diffType::FILE_HEADER;
+
+
+    if(output.startsWith("index")
+       || output.startsWith("diff")
+       || output.startsWith("+++")
+       || output.startsWith("---"))
+    {
+      line->type = GitDiffLine::diffType::HEADER;
+    }
+    else
+    {
+      switch(output.at(0))
+      {
+      case '@':
+        line->type = GitDiffLine::diffType::HEADER;
+        regex.indexIn(line->content);
+        lineNos = regex.capturedTexts();
+        lineNoOld = lineNos.at(1).toInt();
+        lineNoNew = lineNos.at(2).toInt();
+        break;
+      case '+':
+        line->content = line->content.right(line->content.length() - 1);
+        line->type = GitDiffLine::diffType::ADD;
+        line->oldLine = -1;
+        lineNoNew++;
+        break;
+      case '-':
+        line->content = line->content.right(line->content.length() - 1);
+        line->type = GitDiffLine::diffType::REMOVE;
+        line->newLine = -1;
+        lineNoOld++;
+        break;
+      default:
+        line->type = GitDiffLine::diffType::CONTEXT;
+        lineNoOld++;
+        lineNoNew++;
+        break;
+      }
+    }
+
+    list.append(line);
+    output = _impl->process->readLine(1024);
+  }
+
+  return list;
 }
 
 QString gitBin::GitManager::headName()
@@ -55,15 +164,21 @@ QString gitBin::GitManager::headName()
 
 void gitBin::GitManager::stagePath(const QString &path)
 {
-
+  _impl->process->setArguments({"add", path});
+  _impl->process->start();
+  _impl->process->waitForFinished();
 }
 
 void gitBin::GitManager::unstagePath(const QString &path)
 {
-
+  _impl->process->setArguments({"reset", "HEAD", path});
+  _impl->process->start();
+  _impl->process->waitForFinished();
 }
 
 void gitBin::GitManager::commit(const QString &message)
 {
-
+  _impl->process->setArguments({"commit", "-m", message});
+  _impl->process->start();
+  _impl->process->waitForFinished();
 }
