0870578aa4f1151b1677508b8b48d13781db185a
Felix Müller <feelx88@googlemail.com>
2018-06-24 22:24:34 +0200

    Added --all switch to git log gathering.
    # Conflicts:
    #	src/gitinterface.cpp

--- src/gitinterface.cpp
+++ src/gitinterface.cpp
@@ -36,16 +36,14 @@ void GitInterface::reload()
 void GitInterface::log()
 {
   _data->process->setArguments({"log",
-                                  "--reverse",
+                                  "--all",
+                                  "--graph",
+                                  "--full-history",
                                   "--pretty="
-                                  "%H%x0c"
-                                  "%P%x0c"
-                                  "%D%x0c"
-                                  "%cn%x0c"
-                                  "%ce%x0c"
-                                  "%ct%x0c"
-                                  "%s%x0c"
-                                  "%b%x0b",
+                                  "%x0c"
+                                  "%H"
+                                  "%x0c"
+                                  "%s"
                                  });
     _data->process->start(QIODevice::ReadOnly);
     _data->process->waitForFinished();
@@ -62,60 +60,22 @@ void GitInterface::log()
 
       QString buf;
       QByteArray c = _data->process->read(1);
-      while(c != "\v")
+      while(c != "\n")
       {
         buf += c;
         c = _data->process->read(1);
       }
-      _data->process->read(1);
       QStringList parts = buf.split('\f');
 
       GitCommit *commit = new GitCommit(this);
-      commit->id = parts.at(0);
-      commit->branches = parts.at(2).split(", ", QString::SkipEmptyParts);
-      commit->message = parts.at(6);
+      commit->graph = parts.at(0).trimmed();
+      if (parts.length() > 1)
+      {
+        commit->id = parts.at(1);
+        commit->message = parts.at(2);
+      }
+
       list.append(QVariant::fromValue(commit));
-      map.insert(commit->id, commit);
-
-      QStringList parents = parts.at(1).split(" ", QString::SkipEmptyParts);
-
-      for (QString parent : parents)
-      {
-        GitCommit *parentCommit = map.value(parent);
-        if(parentCommit)
-        {
-          commit->parents.append(QVariant::fromValue(parentCommit));
-        }
-      }
-    }
-
-    _data->process->setArguments({"rev-list",
-                                    "--all",
-                                    "--children",
-                                   });
-    _data->process->start(QIODevice::ReadOnly);
-    _data->process->waitForFinished();
-
-    while(true)
-    {
-      if(_data->process->atEnd())
-      {
-        break;
-      }
-
-      QString line = _data->process->readLine();
-      QStringList children = line.split(" ");
-
-      GitCommit *commit = map.value(children.at(0));
-
-      if (commit) {
-        children.pop_front();
-
-        for (QString child : children) {
-          child = child.trimmed();
-          commit->children.append(QVariant::fromValue(map.value(child)));
-        }
-      }
     }
 
     emit logChanged(list);
