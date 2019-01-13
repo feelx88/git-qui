a4e880d990cdd0f1ebdf310cec71b1ec2751892b
Felix Müller <feelx88@googlemail.com>
2019-01-13 12:18:43 +0100

    Made remote branch column font italic.

--- src/components/branchlist/branchlist.cpp
+++ src/components/branchlist/branchlist.cpp
@@ -6,10 +6,13 @@
 struct BranchListPrivate
 {
   QSharedPointer<GitInterface> gitInterface;
+  QFont italicFont;
 
   void connectSignals(BranchList *_this)
   {
     MainWindow *mainWindow = static_cast<MainWindow*>(_this->parent());
+    italicFont = _this->ui->treeWidget->font();
+    italicFont.setItalic(true);
 
     _this->connect(mainWindow, &MainWindow::repositorySwitched, _this, [=](const QSharedPointer<GitInterface> newGitInterface){
       gitInterface->disconnect(gitInterface.get(), &GitInterface::branchesChanged, _this, nullptr);
@@ -20,11 +23,13 @@ struct BranchListPrivate
         _this->ui->treeWidget->clear();
         for (auto branch : branches)
         {
-          _this->ui->treeWidget->addTopLevelItem(new QTreeWidgetItem(_this->ui->treeWidget, {
+          auto item = new QTreeWidgetItem(_this->ui->treeWidget, {
             branch.active ? "➔" : "",
             branch.name,
             branch.upstreamName
-          }));
+          });
+          item->setFont(2, italicFont);
+          _this->ui->treeWidget->addTopLevelItem(item);
         }
         _this->ui->treeWidget->resizeColumnToContents(0);
         _this->ui->treeWidget->resizeColumnToContents(1);
