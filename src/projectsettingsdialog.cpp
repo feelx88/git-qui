d4844457483e9e6d2e70a3330c08dcfd64859f44
Felix Müller <feelx88@googlemail.com>
2019-10-26 23:34:24 +0200

    Renamed widgets according to their function.

--- src/projectsettingsdialog.cpp
+++ src/projectsettingsdialog.cpp
@@ -12,13 +12,13 @@ struct ProjectSettingsDialogImpl
 
   void fillRepositoryList(ProjectSettingsDialog *_this)
   {
-    _this->ui->tableWidget->clearContents();
-    _this->ui->tableWidget->setRowCount(0);
+    _this->ui->repositoryTable->clearContents();
+    _this->ui->repositoryTable->setRowCount(0);
     for (Repository repository : project->repositoryList())
     {
-      int row = _this->ui->tableWidget->rowCount();
-      _this->ui->tableWidget->insertRow(row);
-      _this->ui->tableWidget->setItem(row, 1, new QTableWidgetItem(repository.path.path()));
+      int row = _this->ui->repositoryTable->rowCount();
+      _this->ui->repositoryTable->insertRow(row);
+      _this->ui->repositoryTable->setItem(row, 1, new QTableWidgetItem(repository.path.path()));
     }
   }
 };
@@ -41,41 +41,41 @@ ProjectSettingsDialog::ProjectSettingsDialog(ProjectSettingsDialog::DialogMode d
   {
     ui->buttonBox->button(QDialogButtonBox::Ok)->hide();
     ui->buttonBox->button(QDialogButtonBox::Cancel)->hide();
-    ui->toolButton_2->setEnabled(true);
+    ui->projectPathChooseButton->setEnabled(true);
   }
 
-  ui->lineEdit->setText(_impl->project->name());
-  ui->lineEdit_2->setText(_impl->project->fileName());
+  ui->projectNameEdit->setText(_impl->project->name());
+  ui->projectPathEdit->setText(_impl->project->fileName());
   _impl->fillRepositoryList(this);
 
-  connect(ui->toolButton, &QToolButton::clicked, this, [this]{
+  connect(ui->projectPathChooseButton, &QToolButton::clicked, this, [this]{
     _impl->project->setFileName(QFileDialog::getOpenFileName(
       this,
       "Select project file"
     ));
 
-    ui->lineEdit_2->setText(_impl->project->fileName());
-    ui->toolButton_2->setEnabled(true);
+    ui->projectPathEdit->setText(_impl->project->fileName());
+    ui->addRepositoryButton->setEnabled(true);
   });
 
-  ui->tableWidget->setHorizontalHeaderLabels(QStringList() << "Name" << "Path");
+  ui->repositoryTable->setHorizontalHeaderLabels(QStringList() << "Name" << "Path");
 
-  connect(ui->tableWidget, &QTableWidget::itemSelectionChanged, this, [this]{
-    ui->toolButton_3->setEnabled(ui->tableWidget->currentRow() >= 0);
+  connect(ui->repositoryTable, &QTableWidget::itemSelectionChanged, this, [this]{
+    ui->removeRepositoryButton->setEnabled(ui->repositoryTable->currentRow() >= 0);
   });
 
-  connect(ui->toolButton_2, &QToolButton::clicked, this, [this]{
+  connect(ui->addRepositoryButton, &QToolButton::clicked, this, [this]{
     _impl->project->addRepository();
 
-    int row = ui->tableWidget->rowCount();
-    ui->tableWidget->insertRow(row);
-    ui->tableWidget->setItem(row, 1, new QTableWidgetItem(_impl->project->repositoryList().last().path.path()));
+    int row = ui->repositoryTable->rowCount();
+    ui->repositoryTable->insertRow(row);
+    ui->repositoryTable->setItem(row, 1, new QTableWidgetItem(_impl->project->repositoryList().last().path.path()));
 
     ui->buttonBox->button(QDialogButtonBox::Ok)->setEnabled(true);
   });
 
-  connect(ui->toolButton_3, &QToolButton::clicked, this, [this]{
-    _impl->project->removeRepository(ui->tableWidget->currentRow());
+  connect(ui->removeRepositoryButton, &QToolButton::clicked, this, [this]{
+    _impl->project->removeRepository(ui->repositoryTable->currentRow());
     _impl->fillRepositoryList(this);
   });
 }
