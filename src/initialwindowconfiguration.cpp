36766afcce23d119b7ceb311dfc9ab1b8b70228a
Felix Müller <feelx88@googlemail.com>
2020-04-12 21:50:44 +0200

    Made inital window configuration a little bit easier to read.

--- src/initialwindowconfiguration.cpp
+++ src/initialwindowconfiguration.cpp
@@ -6,67 +6,58 @@
 #include "toolbaractions.hpp"
 #include "components/dockwidget.hpp"
 
+#include "components/repositoryfiles/repositoryfiles.hpp"
+#include "components/diffview/diffview.hpp"
+#include "components/commit/commit.hpp"
+#include "components/repositorylist/repositorylist.hpp"
+#include "components/branchlist/branchlist.hpp"
+#include "components/logview/logview.hpp"
+
+#define ADD_DOCK_WIDGET_WITH_CONFIG(name, target, config) DockWidget::create( \
+  #name, \
+  mainWindow, \
+  target, \
+  QUuid::createUuid().toString(), \
+  config \
+)
+
+#define ADD_DOCK_WIDGET(name, target) DockWidget::create( \
+  #name, \
+  mainWindow, \
+  target, \
+  QUuid::createUuid().toString() \
+)
+
+#define SPLIT_DOCK_WIDGET(target, direction, first, second) target->splitDockWidget( \
+  static_cast<QDockWidget*>(main->children()[first]), \
+  static_cast<QDockWidget*>(main->children()[second]), \
+  Qt::direction \
+);
+
 void InitialWindowConfiguration::create(MainWindow *mainWindow)
 {
   QMainWindow *main = mainWindow->createTab(mainWindow->tr("main"));
-  QMap<QString, QVariant> unstagedConfig = {{"unstaged", true}};
-  DockWidget::create(
-    "RepositoryFiles",
-    mainWindow,
+  ADD_DOCK_WIDGET_WITH_CONFIG(
+    RepositoryFiles,
     main,
-    QUuid::createUuid().toString(),
-    unstagedConfig
+    (QMap<QString, QVariant>({{"unstaged", true}}))
   );
-  QMap<QString, QVariant> stagedConfig = {{"unstaged", false}};
-  DockWidget::create(
-    "RepositoryFiles",
-    mainWindow,
+  ADD_DOCK_WIDGET_WITH_CONFIG(
+    RepositoryFiles,
     main,
-    QUuid::createUuid().toString(),
-    stagedConfig
+    (QMap<QString, QVariant>({{"unstaged", false}}))
   );
-  DockWidget::create(
-    "DiffView",
-    mainWindow,
-    main
-  );
-  DockWidget::create(
-    "Commit",
-    mainWindow,
-    main
-  );
-  DockWidget::create(
-    "RepositoryList",
-    mainWindow,
-    main
-  );
-  DockWidget::create(
-    "BranchList",
-    mainWindow,
-    main
-  );
-  main->splitDockWidget(
-    static_cast<QDockWidget*>(main->children()[1]),
-    static_cast<QDockWidget*>(main->children()[2]),
-    Qt::Vertical
-  );
-  main->splitDockWidget(
-    static_cast<QDockWidget*>(main->children()[3]),
-    static_cast<QDockWidget*>(main->children()[4]),
-    Qt::Vertical
-  );
-  main->splitDockWidget(
-    static_cast<QDockWidget*>(main->children()[5]),
-    static_cast<QDockWidget*>(main->children()[6]),
-    Qt::Vertical
-  );
+  ADD_DOCK_WIDGET(DiffView, main);
+  ADD_DOCK_WIDGET(Commit, main);
+  ADD_DOCK_WIDGET(RepositoryList, main);
+  ADD_DOCK_WIDGET(BranchList, main);
+
+  SPLIT_DOCK_WIDGET(main, Vertical, 1, 2);
+  SPLIT_DOCK_WIDGET(main, Vertical, 3, 4);
+  SPLIT_DOCK_WIDGET(main, Vertical, 5, 6);
 
   QMainWindow *history = mainWindow->createTab(mainWindow->tr("History"));
-  DockWidget::create(
-    "LogView",
-    mainWindow,
-    history
-  );
+  ADD_DOCK_WIDGET(LogView, history);
 
   mainWindow->setEditMode(true);
 
