bf543780b5e2c8b20d86d52288e10617982c0fed
Felix Müller <feelx88@googlemail.com>
2022-08-08 22:11:33 +0200

    Added first version of delegate for drawing the git tree.

--- src/components/logview/logview.cpp
+++ src/components/logview/logview.cpp
@@ -6,11 +6,76 @@
 #include "mainwindow.hpp"
 #include "treewidgetitem.hpp"
 
+#include <QItemDelegate>
+#include <QPainter>
+#include <iostream>
+
+struct LaneInfo {
+  int lane = 0, laneCount = 1;
+};
+
+struct Delegate : public QItemDelegate {
+  QSharedPointer<GitTree> gitTree;
+  QMap<QString, LaneInfo> lanes;
+
+  Delegate(QObject *parent) : QItemDelegate(parent) {}
+
+  void setGitTree(QSharedPointer<GitTree> gitTree) { this->gitTree = gitTree; }
+  void setLanes(QMap<QString, LaneInfo> lanes) { this->lanes = lanes; }
+
+  void paint(QPainter *painter, const QStyleOptionViewItem &option,
+             const QModelIndex &index) const override {
+    QPoint center(option.rect.x() + 9, option.rect.center().y());
+    auto commit = gitTree->commitList().at(index.row());
+
+    drawBackground(painter, option, index);
+
+    for (int parentIndex = 0; parentIndex < commit->parentCommits.size();
+         ++parentIndex) {
+      painter->setPen(QPen(QBrush(Qt::red), 2));
+      painter->drawLine(
+          center, center + QPoint(parentIndex * 24, option.rect.height() / 2));
+    }
+
+    for (int childIndex = 0; childIndex < commit->childCommits.size();
+         ++childIndex) {
+      painter->setPen(QPen(QBrush(Qt::red), 2));
+      painter->drawLine(
+          center, center - QPoint(childIndex * -24, option.rect.height() / 2));
+    }
+
+    LaneInfo laneInfo = lanes.value(commit->id);
+
+    for (int lane = 0; lane < laneInfo.laneCount; ++lane) {
+      painter->setPen(QPen(QBrush(Qt::red), 2));
+      painter->drawLine(
+          center + QPoint(lane * 24, 0) - QPoint(0, option.rect.height() / 2),
+          center + QPoint(lane * 24, 0) + QPoint(0, option.rect.height() / 2));
+
+      if (lane == laneInfo.lane) {
+        painter->setBrush(Qt::blue);
+        painter->setPen(Qt::blue);
+        painter->drawEllipse(center + QPoint(lane * 24, 0), 6, 6);
+      }
+    }
+  }
+
+  QSize sizeHint(const QStyleOptionViewItem &,
+                 const QModelIndex &index) const override {
+    return QSize(index.data(Qt::UserRole + 1).toList().size() * 5 + 10, 0);
+  }
+};
+
 struct LogViewPrivate {
   GitInterface *gitInterface = nullptr;
+  Delegate *graphDelegate;
+  QMap<QString, LaneInfo> lanes;
+
+  LogViewPrivate(LogView *_this) : graphDelegate(new Delegate(_this)) {}
 
   void connectSignals(LogView *_this) {
     _this->ui->treeWidget->setHeaderLabels({
+        _this->tr("Graph"),
         _this->tr("Id"),
         _this->tr("Branches"),
         _this->tr("Message"),
@@ -23,8 +88,10 @@ struct LogViewPrivate {
 DOCK_WIDGET_IMPL(LogView, tr("Log view"))
 
 LogView::LogView(MainWindow *mainWindow)
-    : DockWidget(mainWindow), ui(new Ui::LogView), _impl(new LogViewPrivate) {
+    : DockWidget(mainWindow), ui(new Ui::LogView),
+      _impl(new LogViewPrivate(this)) {
   ui->setupUi(this);
+  ui->treeWidget->setItemDelegateForColumn(0, _impl->graphDelegate);
 
   _impl->connectSignals(this);
 }
@@ -57,23 +124,50 @@ void LogView::onRepositorySwitched(GitInterface *newGitInterface,
                                    QObject *activeRepositoryContext) {
   DockWidget::onRepositorySwitched(newGitInterface, activeRepositoryContext);
   _impl->gitInterface = newGitInterface;
-
   connect(newGitInterface, &GitInterface::logChanged, activeRepositoryContext,
-          [=](const QList<GitCommit> &commits) {
+          [=](QSharedPointer<GitTree> tree) {
             ui->treeWidget->clear();
-            for (const GitCommit &commit : commits) {
+
+            for (const auto &commit : tree->commitList()) {
               TreeWidgetItem *item = new TreeWidgetItem(ui->treeWidget);
-              item->setText(0, commit.id);
-              item->setText(1, commit.branches.join(", "));
-              item->setText(2, commit.message);
-              item->setText(3, commit.author);
-              item->setText(4, commit.date.toString());
+              item->setText(1, commit->id);
+              item->setText(2, commit->branchHeads.join(", "));
+              item->setText(3, commit->message);
+              item->setText(4, commit->author);
+              item->setText(5, commit->date.toString());
 
               ui->treeWidget->addTopLevelItem(item);
+
+              LaneInfo laneInfo = _impl->lanes.value(commit->id),
+                       parentLaneInfo;
+
+              if (commit->childCommits.size() > 0) {
+                laneInfo.lane -= (commit->childCommits.size() - 1);
+                laneInfo.laneCount -= (commit->childCommits.size() - 1);
+              }
+
+              laneInfo.lane = laneInfo.lane >= 0 ? laneInfo.lane : 0;
+              laneInfo.laneCount =
+                  laneInfo.laneCount >= 1 ? laneInfo.laneCount : 1;
+
+              for (int parentIndex = 0;
+                   parentIndex < commit->parentCommits.size(); ++parentIndex) {
+                parentLaneInfo.lane = parentLaneInfo.lane + parentIndex;
+                parentLaneInfo.laneCount =
+                    laneInfo.laneCount + (commit->parentCommits.size() - 1);
+                _impl->lanes.insert(
+                    commit->parentCommits.at(parentIndex).lock()->id,
+                    parentLaneInfo);
+              }
+
+              _impl->lanes.insert(commit->id, laneInfo);
             }
 
-            ui->treeWidget->resizeColumnToContents(0);
-            ui->treeWidget->resizeColumnToContents(2);
+            _impl->graphDelegate->setGitTree(tree);
+            _impl->graphDelegate->setLanes(_impl->lanes);
+
+            ui->treeWidget->resizeColumnToContents(1);
             ui->treeWidget->resizeColumnToContents(3);
+            ui->treeWidget->resizeColumnToContents(4);
           });
 }
